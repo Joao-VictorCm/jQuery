c1aafa312040618dc336ac6c60d567c4af7e7bd0
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-22 21:46:13 -0300

    Funções basicas

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("funcionando")
+// Links uteis
+// CDN = https://developers.google.com/speed/libraries?hl=pt-br#jquery
+// https://jquery.com/
+
+$("h1").addClass("big-title class02")
+ 
+
+// verificando se o elemento tem a classe
+// caso sim ele retorna true
+
+$("h1").hasClass("class02")
