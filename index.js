f88d7d0cde09348f7be16ebc82adf7d1a2929279
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-22 22:32:10 -0300

    Add eventos e funções

--- index.js
+++ index.js
@@ -2,20 +2,51 @@
 // CDN = https://developers.google.com/speed/libraries?hl=pt-br#jquery
 // https://jquery.com/
 
-$("h1").addClass("big-title class02")
+
+
+ $("h1").addClass("big-title class02")
  
 
-// verificando se o elemento tem a classe
-// caso sim ele retorna true
+// // verificando se o elemento tem a classe
+// // caso sim ele retorna true
 
 $("h1").hasClass("class02")
 
 
-// alterando textos
+// // alterando textos
 
 $("h1").text("Bye")
 
 
-//Adicionado HTML
+// //Adicionado HTML
 
 $("button").html("<em>Hey</em>")
+
+// //Alterando atributos
+
+$("a").attr("href", "https://www.yahoo.com")
+
+
+
+
+// //Add funções
+// Eventos: https://developer.mozilla.org/en-US/docs/Web/Events
+
+$("h1").click(function () {
+    $("h1").css("color", "red")
+})
+
+$("button").click(function(){
+    $("h1").css("color", "green")
+})
+
+$("input").keypress(function (event) {
+
+    // var i = event.key
+    $("h1").text(event.key)
+
+})
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "purple")
+})
